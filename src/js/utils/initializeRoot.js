c9ebc6f7cddc02fbf1f8c2c007d81d1da27181be
Dag Stuan <D.Stuan@gmail.com>
2017-10-19 07:22:56 +0200

    Rename pageComponent to rootComponent

--- src/js/utils/initializeRoot.js
+++ src/js/utils/initializeRoot.js
@@ -2,6 +2,6 @@ export default function initializeRoot(store, domId, rootName) {
   const rootEl = document.getElementById(domId);
   if (rootEl) {
     import(/* webpackChunkName: "[request]" */ `../roots/${rootName}.js`)
-      .then(pageComponent => pageComponent.default(store, rootEl));
+      .then(rootComponent => rootComponent.default(store, rootEl));
   }
 }
